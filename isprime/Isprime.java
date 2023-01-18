cd3c1a6c1180624604b9eda022cdb6b10ad20643
Gabriel <95552879+gabflag@users.noreply.github.com>
2023-01-18 11:09:40 -0300

    Update Isprime.java

--- isprime/Isprime.java
+++ isprime/Isprime.java
@@ -73,6 +73,8 @@ public class Isprime {
     	private static boolean isPrime(int n) {
 		if(n == 2) return true;
 		if(n % 2 == 0) return false;
+		
+		// Adicionado de 2 em dois para excluir números pares
 		for (int i = 3; i <= Math.sqrt(n); i += 2) {
 		    if (n % i == 0) {
 			return false;
